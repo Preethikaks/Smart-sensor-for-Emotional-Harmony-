9f7343717fcf8f078c24e43b6e564ac0a4cdf668
Preethikaks <164054755+Preethikaks@users.noreply.github.com>
2025-07-20 16:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="7019838679",
+        password="",
         database="emotion_music_db"
     )
 
